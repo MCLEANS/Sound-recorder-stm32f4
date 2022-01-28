57e5d545c81616a0838bc1d775f2b380baab2c12
MCLEANS <jackagwaya@gmail.com>
2022-01-28 16:18:31 +0300

    added adc configuration

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -41,6 +41,8 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
 SD_HandleTypeDef hsd;
 DMA_HandleTypeDef hdma_sdio_rx;
 DMA_HandleTypeDef hdma_sdio_tx;
@@ -63,6 +65,7 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_SDIO_SD_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -104,29 +107,33 @@ int main(void)
   MX_SDIO_SD_Init();
   MX_USART1_UART_Init();
   MX_FATFS_Init();
+  MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
 
   if(f_mount(&SDFatFS, " ", 0) != FR_OK){
-  	 DEBUG_CN("Error mounting SD\n");
+  	 DEBUG_CN("Error mounting SD \n");
    }
   else{
 	  DEBUG_CN("SD mounted successfully\n");
 	  res =  f_open(&SDFile, "wav.txt", FA_OPEN_ALWAYS | FA_WRITE | FA_READ);
 	  if(res == FR_OK){
-		  DEBUG_CN("File opened successfully\n");
+		  DEBUG_CN("File opened successfully \n");
 	  }
 	  else{
 		  char error_msg[4];
 		  itoa(res,error_msg,10);
 		  DEBUG_CN("Error Opening File with Error : ");
 		  DEBUG_CN(error_msg);
-		  DEBUG_CN('\n');
+		  DEBUG_CN("\n");
 		  HAL_Delay(500);
 		  f_close(&SDFile);
 	  }
   }
 
   f_mount(0, "", 0);
+
+  DEBUG_CN("Initialization Complete \n");
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -134,6 +141,13 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	  /* Read raw ADC value */
+	  HAL_ADC_Start(&hadc1);
+	  uint16_t adc_value = HAL_ADC_GetValue(&hadc1);
+	  char adc_value_str[5] = {};
+	  itoa(adc_value,adc_value_str,10);
+	  DEBUG_CN(adc_value_str);
+	  DEBUG_CN("\n");
 
     /* USER CODE BEGIN 3 */
   }
@@ -181,6 +195,56 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
